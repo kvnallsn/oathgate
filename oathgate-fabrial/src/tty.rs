6d678293202b6280caf4a4e812fdcaa14c26d676
Kevin Allison <kvnallsn@gmail.com>
2024-06-08 10:28:02 -0500

    oathgate-fabrial: support tty resizing

--- oathgate-fabrial/src/tty.rs
+++ oathgate-fabrial/src/tty.rs
@@ -1,9 +1,17 @@
 //! TTY functions
 
-use std::{io, os::fd::{AsRawFd, OwnedFd, RawFd}};
+use std::{
+    io,
+    os::fd::{AsRawFd, OwnedFd, RawFd},
+};
 
 use mio::{unix::SourceFd, Events, Interest, Poll, Token};
-use nix::{pty::{forkpty, ForkptyResult}, sys::{signal::Signal, socket::MsgFlags}};
+use nix::{
+    ioctl_write_ptr_bad,
+    libc::TIOCSWINSZ,
+    pty::{forkpty, ForkptyResult, Winsize},
+    sys::{signal::Signal, socket::MsgFlags},
+};
 
 /// Performs a non-blocking recv call, returning 0 if it would block (i.e., EAGAIN / EWOULDBLOCK)
 macro_rules! non_blocking_recv {
@@ -13,13 +21,19 @@ macro_rules! non_blocking_recv {
             Err(nix::errno::Errno::EWOULDBLOCK) => Ok(0),
             Err(error) => Err(error),
         }
-    }}
+    }};
 }
 
 /// A `SockTTY` represents a TTY connected over a socket
 pub struct SockTTY {
     client: RawFd,
     tty: OwnedFd,
+}
+
+pub enum SocketAction {
+    WriteToTTY,
+    WouldBlock,
+    Continue,
 }
 
 impl SockTTY {
@@ -34,25 +48,29 @@ impl SockTTY {
 
                 tracing::error!("execve returned!");
                 std::process::exit(0);
-            },
+            }
             ForkptyResult::Parent { child, master } => {
-                let stty = Self { client, tty: master };
+                let stty = Self {
+                    client,
+                    tty: master,
+                };
 
-                let _handle = std::thread::Builder::new().name(String::from("tty-thread")).spawn(move || {
-                    if let Err(error) = stty.run() {
-                        tracing::warn!(?error, %child, "tty died");
-                    }
+                let _handle = std::thread::Builder::new()
+                    .name(String::from("tty-thread"))
+                    .spawn(move || {
+                        if let Err(error) = stty.run() {
+                            tracing::warn!(?error, %child, "tty died");
+                        }
 
-                    // reap the child
-                    tracing::debug!(%child, "attempting to reap child");
-                    match nix::sys::signal::kill(child, Signal::SIGKILL)
-                        .and_then(|_| nix::sys::wait::waitpid(child, None)) {
-                        Ok(_) => tracing::debug!(%child, "child reaped"),
-                        Err(error) => tracing::warn!(?error, "unable to signal child"),
-                    }
-
-                    
-                })?;
+                        // reap the child
+                        tracing::debug!(%child, "attempting to reap child");
+                        match nix::sys::signal::kill(child, Signal::SIGKILL)
+                            .and_then(|_| nix::sys::wait::waitpid(child, None))
+                        {
+                            Ok(_) => tracing::debug!(%child, "child reaped"),
+                            Err(error) => tracing::warn!(?error, "unable to signal child"),
+                        }
+                    })?;
 
                 Ok(())
             }
@@ -68,23 +86,27 @@ impl SockTTY {
         let mut poller = Poll::new()?;
         let mut events = Events::with_capacity(MAX_EVENTS);
 
-        poller.registry().register(&mut SourceFd(&self.client), TOKEN_SOCK, Interest::READABLE)?;
-        poller.registry().register(&mut SourceFd(&self.tty.as_raw_fd()), TOKEN_TTY, Interest::READABLE)?;
+        poller
+            .registry()
+            .register(&mut SourceFd(&self.client), TOKEN_SOCK, Interest::READABLE)?;
+        poller.registry().register(
+            &mut SourceFd(&self.tty.as_raw_fd()),
+            TOKEN_TTY,
+            Interest::READABLE,
+        )?;
 
         let mut buf = [0u8; BUFFER_SIZE];
         let mut msg = Vec::with_capacity(BUFFER_SIZE * 4);
         while let Ok(_) = poller.poll(&mut events, None) {
             for event in &events {
                 match event.token() {
-                    TOKEN_SOCK => {
-                        'sock: loop {
-                            match self.read_from_socket(&mut buf, &mut msg) {
-                                Ok(_) => {
-                                    tracing::debug!("read socket msg: {:02x?}", msg);
-                                    self.write_to_tty(&msg)?;
-                                },
-                                Err(err) if err.kind() == io::ErrorKind::WouldBlock => break 'sock,
-                                Err(err) => return Err(err)?,
+                    TOKEN_SOCK => 'sock: loop {
+                        match self.read_from_socket(&mut buf, &mut msg)? {
+                            SocketAction::WouldBlock => break 'sock,
+                            SocketAction::Continue => (),
+                            SocketAction::WriteToTTY => {
+                                tracing::debug!("read socket msg: {:02x?}", msg);
+                                self.write_to_tty(&msg)?;
                             }
                         }
                     },
@@ -92,7 +114,7 @@ impl SockTTY {
                         self.read_from_tty(&mut buf, &mut msg)?;
                         tracing::trace!("read tty msg: {:02x?}", msg);
                         self.write_to_socket(&msg)?;
-                    },
+                    }
                     Token(token) => tracing::debug!(%token, "unknown mio token"),
                 }
             }
@@ -111,7 +133,7 @@ impl SockTTY {
     /// Message Types:
     /// - 0x01: TTY data (aka user input)
     /// - 0x02: Resize TTY
-    pub fn read_from_socket(&self, buf: &mut [u8], msg: &mut Vec<u8>) -> io::Result<()> {
+    pub fn read_from_socket(&self, buf: &mut [u8], msg: &mut Vec<u8>) -> io::Result<SocketAction> {
         const MSG_TY_ZERO: u8 = 0x00;
         const MSG_TY_DATA: u8 = 0x01;
         const MSG_TY_TTYRESIZE: u8 = 0x02;
@@ -123,17 +145,20 @@ impl SockTTY {
         tracing::trace!("got msg type: {:02x}", msg_type[0]);
 
         match msg_type[0] {
-            MSG_TY_ZERO => {
-                return Err(io::Error::new(io::ErrorKind::WouldBlock, "no more data"))?;
-            },
+            MSG_TY_ZERO => Ok(SocketAction::WouldBlock),
             MSG_TY_DATA => {
                 self.handle_socket_data(buf, msg)?;
-            },
-            MSG_TY_TTYRESIZE => (),
-            msgtype => tracing::warn!(msgtype, "unknown message type"),
-        };
-
-        Ok(())
+                Ok(SocketAction::WriteToTTY)
+            }
+            MSG_TY_TTYRESIZE => {
+                self.handle_tty_resize()?;
+                Ok(SocketAction::Continue)
+            }
+            msgtype => {
+                tracing::warn!(msgtype, "unknown message type");
+                Ok(SocketAction::Continue)
+            }
+        }
     }
 
     /// Handles a data message (aka msg code: 0x01)
@@ -169,6 +194,38 @@ impl SockTTY {
                     length -= sz;
                 }
             }
+        }
+
+        Ok(())
+    }
+
+    /// Handles a TTY resize (i.e., 0x02) message
+    ///
+    /// Format:
+    /// | rows (u16) | cols (u16) |
+    fn handle_tty_resize(&self) -> io::Result<()> {
+        ioctl_write_ptr_bad!(tiocswinsz, TIOCSWINSZ, Winsize);
+
+        let mut buf = [0u8; 4];
+        non_blocking_recv!(self.client, &mut buf)?;
+
+        let rows = u16::from_le_bytes([buf[0], buf[1]]);
+        let cols = u16::from_le_bytes([buf[2], buf[3]]);
+
+        if rows == 0 || cols == 0 {
+            return Err(io::Error::other("invalid resize message"))?;
+        }
+
+        let winsz = Winsize {
+            ws_row: rows,
+            ws_col: cols,
+            ws_xpixel: 0,
+            ws_ypixel: 0,
+        };
+
+        tracing::debug!(rows, cols, "attempting to resize tty");
+        unsafe {
+            tiocswinsz(self.tty.as_raw_fd(), &winsz as *const Winsize)?;
         }
 
         Ok(())
